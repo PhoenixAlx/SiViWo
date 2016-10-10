521727f96eeb1329a7eefc0f1c68cc22fbc7a67b
PhoenixAlx <raskalakabra@gmail.com>
2016-10-10 13:25:17 +0100

    my changes

--- model_0/lib/networkClass.py
+++ model_0/lib/networkClass.py
@@ -1,1 +1,45 @@
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#  
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following disclaimer
+#    in the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of the  nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#  
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#  
+import networkx as nx
+class Community:
+    """This is the class community, a set of  n agents and edges"""
+    def __init__(self, n):
+        self.G = nx.Graph();
+        self.numberNodes=n;
+        nodes= range(1,n+1);
+        self.G.add_nodes_from(nodes);
+    def setFullyConnect(self):
+        """This function creates a  fully connected network """
+        #for each node we create an array whith the link (node origin, another node)
+        for i in range(1,self.numberNodes+1):
+            nodeLink=[];
+            for j in range(1,self.numberNodes+1):
+                if (i!=j):
+                    nodeLink.append((i,j));
+            self.G.add_edges_from(nodeLink);
+        return 0;
 
